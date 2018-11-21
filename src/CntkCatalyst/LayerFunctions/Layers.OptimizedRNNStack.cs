d6f76d710c2ec7d9c0b55c908b281c73f65820c0
Mads Dabros <mdabros@users.noreply.github.com>
2018-11-21 21:22:17 +0100

    0.1.0-alpha02: Ch_62_Understanding_Recurrent_Neural_Networks (#2)
    * Add Layers.OptimizedRNNStack
    
    * Refactor names
    
    * Change dropout to accept in as seed
    
    * Fix weightshape
    
    * Add Ch_62_Understanding_Recurrent_Neural_Networks
    
    * Increment version

---
+++ src/CntkCatalyst/LayerFunctions/Layers.OptimizedRNNStack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using CNTK;
+
+namespace CntkCatalyst.LayerFunctions
+{
+    /// <summary>
+    /// Layer operations for CNTK
+    /// </summary>
+    public static partial class Layers
+    {
+        static class RecurrentOperators
+        {
+            public const string LSTM = "lstm";
+            public const string GRU = "gru";
+            public const string RNN_Tanh = "rnnTanh";
+            public const string RNN_ReLU = "rnnReLU";
+        }
+        
+        public static Function LSTMStack(this Function input, int units, int layerCount, 
+            CNTKDictionary weightInitializer, 
+            bool bidirectional,
+            DeviceDescriptor device,
+            DataType dataType,
+            string name = "")
+        {                
+            return CreateOptimizedRNNStack(input, RecurrentOperators.LSTM, units, layerCount, 
+                weightInitializer, bidirectional, device, dataType, name);
+        }
+
+        public static Function GRUStack(this Function input, int units, int layerCount,
+            CNTKDictionary weightInitializer,
+            bool bidirectional,
+            DeviceDescriptor device,
+            DataType dataType,
+            string name = "")
+        {
+            return CreateOptimizedRNNStack(input, RecurrentOperators.GRU, units, layerCount,
+                weightInitializer, bidirectional, device, dataType, name);
+        }
+
+        public static Function SimpleReluRNNStack(this Function input, int units, int layerCount,
+            CNTKDictionary weightInitializer,
+            bool bidirectional,
+            DeviceDescriptor device,
+            DataType dataType,
+            string name = "")
+        {
+            return CreateOptimizedRNNStack(input, RecurrentOperators.RNN_ReLU, units, layerCount,
+                weightInitializer, bidirectional, device, dataType, name);
+        }
+
+        public static Function SimpleTanhRNNStack(this Function input, int units, int layerCount,
+            CNTKDictionary weightInitializer,
+            bool bidirectional,
+            DeviceDescriptor device,
+            DataType dataType,
+            string name = "")
+        {
+            return CreateOptimizedRNNStack(input, RecurrentOperators.RNN_Tanh, units, layerCount,
+                weightInitializer, bidirectional, device, dataType, name);
+        }
+
+        static Function CreateOptimizedRNNStack(Function input, string recurrentOperator, int units,
+            int layerCount,
+            CNTKDictionary weightInitializer,
+            bool bidirectional,
+            DeviceDescriptor device,
+            DataType dataType,
+            string name)
+        {
+            if (device.Type != DeviceKind.GPU)
+            {
+                throw new NotSupportedException($"OptimizedRNNStack only supports GPU. Device was: {device.Type}");
+            }
+
+            // TODO: Investigate initialization:
+            // All weights are contained in a single matrix that should have hiddenDims rows 
+            // and as many columns as needed to hold all parameters. Since this can be cumbersome to determine, 
+            // you can have the dimension inferred automatically. 
+            // To make sure that random initialization uses the correct fan-in, specify initOutputRank=-1:
+
+            var weighthape = new int[] { units, NDShape.InferredDimension };
+            var weights = new Parameter(weighthape, dataType, weightInitializer, device);
+
+            return CNTKLib.OptimizedRNNStack(input, weights, (uint)units, (uint)layerCount,
+                bidirectional, recurrentOperator, name);
+        }
+    }
+}
