e402543737ef3e6c01cd5c08c67703cd762bd0e5
mdabros <mdabros@users.noreply.github.com>
2018-12-18 19:50:20 +0100

    Add CntkArithmetic for fluent arithmetic operators. WIP

---
+++ src/CntkCatalyst/CntkArithmetic.cs
@@ -0,0 +1,37 @@
+﻿using CNTK;
+
+namespace CntkCatalyst
+{
+    public static class CntkArithmetic
+    {
+        public static Function ElementTimes(this Function leftOperand, Function rightOperand)
+        {
+            return CNTKLib.ElementTimes(leftOperand, rightOperand);
+        }
+
+        public static Function ElementTimes(this Variable leftOperand, Variable rightOperand)
+        {
+            return CNTKLib.ElementTimes(leftOperand, rightOperand);
+        }
+
+        public static Function Minus(this Function leftOperand, Function rightOperand)
+        {
+            return CNTKLib.Minus(leftOperand, rightOperand);
+        }
+
+        public static Function Minus(this Variable leftOperand, Variable rightOperand)
+        {
+            return CNTKLib.Minus(leftOperand, rightOperand);
+        }
+
+        public static Function Plus(this Function leftOperand, Function rightOperand)
+        {
+            return CNTKLib.Plus(leftOperand, rightOperand);
+        }
+
+        public static Function Plus(this Variable leftOperand, Variable rightOperand)
+        {
+            return CNTKLib.Plus(leftOperand, rightOperand);
+        }
+    }
+}
