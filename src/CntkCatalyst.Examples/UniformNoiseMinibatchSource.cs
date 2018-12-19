3aa1c5cecf28c1c06b6cfa46daa557154936ca06
mdabros <mdabros@users.noreply.github.com>
2018-12-19 21:48:53 +0100

    Move UniformNoiseMinibatchSource to base namespace, since it might also be useful for other examples

--- src/CntkCatalyst.Examples/UniformNoiseMinibatchSource.cs
+++ src/CntkCatalyst.Examples/UniformNoiseMinibatchSource.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CNTK;
 
-namespace CntkCatalyst.Examples.GenerativeModels
+namespace CntkCatalyst.Examples
 {
     public class UniformNoiseMinibatchSource : IMinibatchSource
     {
