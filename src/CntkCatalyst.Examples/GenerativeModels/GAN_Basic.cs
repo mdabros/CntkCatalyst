f621397ee763b873ea979ff6a5c6d45754564238
mdabros <mdabros@users.noreply.github.com>
2018-12-19 19:16:34 +0100

    use noise and composite source in basic gan example

--- src/CntkCatalyst.Examples/GenerativeModels/GAN_Basic.cs
+++ src/CntkCatalyst.Examples/GenerativeModels/GAN_Basic.cs
@@ -71,14 +71,16 @@ namespace CntkCatalyst.Examples.GenerativeModels
                         { discriminatorInputScaled.Output, generatorNetwork.Output },
                     });
 
-            var nameToVariable = new Dictionary<string, Variable>
-            {
-                { "features", discriminatorInput },
-            };
+            // Create minibatch source for providing the real images.
+            var imageNameToVariable = new Dictionary<string, Variable> { { "features", discriminatorInput } };
+            var imageMinibatchSource = CreateMinibatchSource(trainFilePath, imageNameToVariable, randomize: true);
 
-            // The order of the training data is randomize.
-            var train = CreateMinibatchSource(trainFilePath, nameToVariable, randomize: true);
-            var trainingSource = new CntkMinibatchSource(train, nameToVariable);
+            // Create minibatch source for providing the noise.
+            var noiseNameToVariable = new Dictionary<string, Variable> { { "noise", generatorInput } };
+            var noiseMinibatchSource = new UniformNoiseMinibatchSource(noiseNameToVariable, min: -1.0f, max: 1.0f, seed: random.Next());
+            
+            // Combine both sources in the composite minibatch source.
+            var compositeMinibatchSource = new CompositeMinibatchSource(imageMinibatchSource, noiseMinibatchSource);
 
             // setup generator loss: 1.0 - C.log(D_fake)
             var generatorLossFunc = CNTKLib.Minus(Constant.Scalar(1.0f, device), 
@@ -91,7 +93,7 @@ namespace CntkCatalyst.Examples.GenerativeModels
             var generatorFitter = CreateFitter(generatorNetwork, generatorLossFunc, device);
             var discriminatorFitter = CreateFitter(discriminatorNetwork, discriminatorLossFunc, device);
 
-            int epochs = 700;
+            int epochs = 10;
             int batchSize = 1024;
             int discriminitorSteps = 1;
 
@@ -101,33 +103,25 @@ namespace CntkCatalyst.Examples.GenerativeModels
                 // Fit discriminator for k steps.
                 for (int k = 0; k < discriminitorSteps; k++)
                 {
-                    var minibatchItems = trainingSource.GetNextMinibatch(batchSize, device);
+                    // Discriminator needs both real images and noise, 
+                    // so uses the composite minibatch source.
+                    var minibatchItems = compositeMinibatchSource.GetNextMinibatch(batchSize, device);
                     var discriminatorMinibatch = minibatchItems.minibatch;
                     isSweepEnd = minibatchItems.isSweepEnd;
 
-                    if (!isSweepEnd) // only fit when !isSweepEnd, to have full batch sizes.
+                    if (!isSweepEnd) // Only fit when !isSweepEnd, to have full batch sizes.
                     {
-                        // Add noise data to minibatch
-                        var discriminatorNoiseBatchValue = GANUtilities.CreateNoiseSamplesValue(
-                            random, batchSize, ganeratorInputShape, device);
-
-                        discriminatorMinibatch.Add(generatorInput, discriminatorNoiseBatchValue);
                         discriminatorFitter.FitNextStep(discriminatorMinibatch, batchSize);
-
-                        discriminatorNoiseBatchValue.Dispose();
                     }
+
+                    DisposeValues(discriminatorMinibatch);
                 }
 
-                var generatorNoiseBatchValue = GANUtilities.CreateNoiseSamplesValue(
-                    random, batchSize, ganeratorInputShape, device);
-
-                var generatorMinibatch = new Dictionary<Variable, Value>
-                {
-                    { generatorInput,  generatorNoiseBatchValue}
-                };
-
+                // Generator only needs noise images, 
+                // so uses the noise mini batch source separately.
+                var (generatorMinibatch, sweep) = noiseMinibatchSource.GetNextMinibatch(batchSize, device);
                 generatorFitter.FitNextStep(generatorMinibatch, batchSize);
-                generatorNoiseBatchValue.Dispose();
+                DisposeValues(generatorMinibatch);
 
                 if (isSweepEnd)
                 {
@@ -137,7 +131,6 @@ namespace CntkCatalyst.Examples.GenerativeModels
                     var discriminatorCurrentLoss = discriminatorFitter.CurrentLoss;
                     discriminatorFitter.ResetLossAndMetricAccumulators();
 
-
                     var traceOutput = $"Epoch: {epoch + 1:000} Generator Loss = {generatorCurrentLoss:F8}, Discriminator Loss = {discriminatorCurrentLoss:F8}";
 
                     ++epoch;
@@ -146,22 +139,18 @@ namespace CntkCatalyst.Examples.GenerativeModels
                 }
             }
 
-            // Sample images from generator.
+            // Sample 6x6 images from generator.
             var predictor = new Predictor(generatorNetwork, device);
-            var generatorNoiseExampleBatchValue = GANUtilities.CreateNoiseSamplesValue(
-                random, 1, ganeratorInputShape, device);
+            var samples = 6 * 6;
+            var batch = noiseMinibatchSource.GetNextMinibatch(samples, device);
+            var examples = batch.minibatch;
 
-            var generatorExampleInput = new Dictionary<Variable, Value>
-            {
-                { generatorInput,  generatorNoiseExampleBatchValue}
-            };
-
-            var image = predictor.PredictNextStep(generatorExampleInput);
+            var image = predictor.PredictNextStep(examples);
             var imageData = image.SelectMany(t => t).ToArray();
 
             // Show examples
             var app = new System.Windows.Application();
-            var window = new PlotWindowBitMap("Generated Image", imageData, 28, 28, 1, true);
+            var window = new PlotWindowBitMap("Generated Images", imageData, 28, 28, 1, true);
             window.Show();
             app.Run(window);
         }
@@ -199,7 +188,7 @@ namespace CntkCatalyst.Examples.GenerativeModels
             return discriminatorNetwork;
         }
 
-        MinibatchSource CreateMinibatchSource(string mapFilePath, Dictionary<string, Variable> nameToVariable,
+        CntkMinibatchSource CreateMinibatchSource(string mapFilePath, Dictionary<string, Variable> nameToVariable,
             bool randomize)
         {
             var streamConfigurations = new List<StreamConfiguration>();
@@ -216,7 +205,15 @@ namespace CntkCatalyst.Examples.GenerativeModels
                 MinibatchSource.InfinitelyRepeat,
                 randomize);
 
-            return minibatchSource;
+            return new CntkMinibatchSource(minibatchSource, nameToVariable);
+        }
+
+        void DisposeValues(IDictionary<Variable, Value> minibatch)
+        {
+            foreach (var value in minibatch.Values)
+            {
+                value.Dispose();
+            }
         }
     }
 }
