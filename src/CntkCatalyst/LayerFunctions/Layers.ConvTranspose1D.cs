c26d4fd96fc6db445fb636519f2d60d8dee870cc
mdabros <mdabros@users.noreply.github.com>
2018-12-21 14:32:49 +0100

    Add ConvTranspose1D

---
+++ src/CntkCatalyst/LayerFunctions/Layers.ConvTranspose1D.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CNTK;
+
+namespace CntkCatalyst.LayerFunctions
+{
+    /// <summary>
+    /// Layer operations for CNTK
+    /// </summary>
+    public static partial class Layers
+    {
+        public static Function ConvTranspose1D(this Function input,
+            int filterShape,
+            int filterCount,
+            int filterStride,
+            Padding padding,
+            int outputShape,
+            CNTKDictionary weightInitializer,
+            CNTKDictionary biasInitializer,
+            DeviceDescriptor device,
+            DataType dataType)
+        {
+            // Notice that the order of the filter arguments
+            // are different compared to conventional convolution.
+            var filterSizes = new int[]
+            {
+                filterShape,
+                filterCount,
+                NDShape.InferredDimension // Infer number of channels in input.
+            };
+
+            var filterStrides = new int[]
+            {
+                filterStride
+            };
+
+            var outputSizes = new int[]
+            {
+                outputShape,
+                filterCount,
+            };
+
+            return ConvTranspose(input, filterSizes, filterCount, filterStrides,
+                padding, outputSizes, weightInitializer, biasInitializer,
+                device, dataType);
+        }
+    }
+}
