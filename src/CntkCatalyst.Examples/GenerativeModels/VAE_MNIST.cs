22f6553860c5442483b23d89b7710c2ee6f49241
mdabros <mdabros@users.noreply.github.com>
2018-12-22 15:19:26 +0100

    Fix nameToVariable mapping and add visualization

--- src/CntkCatalyst.Examples/GenerativeModels/VAE_MNIST.cs
+++ src/CntkCatalyst.Examples/GenerativeModels/VAE_MNIST.cs
@@ -51,17 +51,17 @@ namespace CntkCatalyst.Examples.GenerativeModels
             var z_mean = encoderNetwork.Dense(latentSize, weightInit(), biasInit, device, dataType);
             var z_log_var = encoderNetwork.Dense(latentSize, weightInit(), biasInit, device, dataType);
             var epsilon = CNTKLib.NormalRandom(new int[] { latentSize }, dataType);
-            var z = CNTKLib.Plus(z_mean, CNTKLib.ElementTimes(CNTKLib.Exp(z_log_var), epsilon));
+            var z = CNTKLib.Plus(z_mean, CNTKLib.ElementTimes(CNTKLib.Exp(z_log_var), epsilon), "decoderInputNode");
 
             // Setup decoder            
             var decoderNetwork = Decoder(z, weightInit, biasInit, device, dataType);
 
             // Create minibatch source for providing the real images.
-            var nameToVariable = new Dictionary<string, Variable> { { "features", scaledEncoderInput } };
+            var nameToVariable = new Dictionary<string, Variable> { { "features", decoderNetwork.Arguments[0] } };
             var trainMinibatchSource = CreateMinibatchSource(trainFilePath, nameToVariable, randomize: true);
             var testMinibatchSource = CreateMinibatchSource(testFilePath, nameToVariable, randomize: false);
 
-            // regularization metric
+            // Regularization metric
             var square_ = CNTKLib.Square(z_mean);
             var exp_ = CNTKLib.Exp(z_log_var);
             var constant_1 = Constant.Scalar(dataType, 1.0);
@@ -71,7 +71,7 @@ namespace CntkCatalyst.Examples.GenerativeModels
             var constant_2 = Constant.Scalar(dataType, -5e-4);
             var regularization_metric = CNTKLib.ElementTimes(constant_2, CNTKLib.ReduceMean(diff_, Axis.AllStaticAxes()));
 
-            // overall loss function
+            // Overall loss function
             var crossentropy_loss = Losses.BinaryCrossEntropy(decoderNetwork.Output, scaledEncoderInput);
             crossentropy_loss = CNTKLib.ReduceMean(crossentropy_loss, Axis.AllStaticAxes());
             var loss = CNTKLib.Plus(crossentropy_loss, regularization_metric);
@@ -79,30 +79,36 @@ namespace CntkCatalyst.Examples.GenerativeModels
             // Setup trainer.
             var learner = Learners.Adam(decoderNetwork.Parameters(), learningRate: 0.001, momentum: 0.9);
             var trainer = Trainer.CreateTrainer(decoderNetwork, loss, loss, new List<Learner> { learner });
-            // create model.
+            // Create model.
             var model = new Model(trainer, decoderNetwork, dataType, device);
 
             Trace.WriteLine(model.Summary());
 
-            int epochs = 10;
-            int batchSize = 16;
+            // Train the model.
+            model.Fit(trainMinibatchSource, batchSize: 16, epochs: 10,
+                validationMinibatchSource: testMinibatchSource);
 
-            model.Fit(trainMinibatchSource, batchSize, epochs, testMinibatchSource);
+            //// Sample 15x15 images from the latent space.
             
-            //// Sample 6x6 images from generator.
-            //var samples = 6 * 6;
-            //var batch = noiseMinibatchSource.GetNextMinibatch(samples, device);
-            //var noise = batch.minibatch;
+            // Setup decoder input for prediction.
+            var decoderInputNode = decoderNetwork.FindByName("decoderInputNode");
+            var decoderInputVariable = Variable.InputVariable(decoderInputNode.Output.Shape, dataType);
+            var replacements = new Dictionary<Variable, Variable>() { { decoderInputNode, decoderInputVariable } };
+            var decoder = decoderNetwork.Clone(ParameterCloningMethod.Freeze, replacements);
 
-            //var predictor = new Predictor(encoderNetwork, device);
-            //var images = predictor.PredictNextStep(noise);
-            //var imagesData = images.SelectMany(t => t).ToArray();
+            // Sample 15x15 samples from the latent space
+            var minibatch = SampleMinibatchForGrid(device, decoderInputVariable, gridSize: 15);
 
-            //// Show examples
-            //var app = new Application();
-            //var window = new PlotWindowBitMap("Generated Images", imagesData, 28, 28, 1, true);
-            //window.Show();
-            //app.Run(window);
+            // Transform from latent space to images using the decoder.
+            var predictor = new Predictor(decoder, device);
+            var images = predictor.PredictNextStep(minibatch);
+            var imagesData = images.SelectMany(t => t).ToArray();
+
+            // Show the example images.
+            var app = new Application();
+            var window = new PlotWindowBitMap("Generated Images", imagesData, 28, 28, 1, true);
+            window.Show();
+            app.Run(window);
         }
 
         Function Encoder(Function input, Func<CNTKDictionary> weightInit, CNTKDictionary biasInit,
@@ -136,6 +142,7 @@ namespace CntkCatalyst.Examples.GenerativeModels
                 .Reshape(NDShape.CreateNDShape(new int[] { 14, 14, 64 }))
 
                 .ConvTranspose2D((3, 3), 32, (2, 2), Padding.Zeros, (28, 28), weightInit(), biasInit, device, dataType)
+                .ReLU()
 
                 .Conv2D((3, 3), 1, (1, 1), Padding.Zeros, weightInit(), biasInit, device, dataType)
                 .Sigmoid();
@@ -161,6 +168,28 @@ namespace CntkCatalyst.Examples.GenerativeModels
                 randomize);
 
             return new CntkMinibatchSource(minibatchSource, nameToVariable);
+        }
+
+        static Dictionary<Variable, Value> SampleMinibatchForGrid(DeviceDescriptor device, Variable decoderInputVariable, int gridSize)
+        {
+            var data = new float[gridSize * gridSize * 2];
+            var sample_start = -2f;
+            var sample_interval_width = 4f;
+            for (int i = 0, pos = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    data[pos++] = sample_start + (sample_interval_width / (gridSize - 1)) * i;
+                    data[pos++] = sample_start + (sample_interval_width / (gridSize - 1)) * j;
+                }
+            }
+            var value = Value.CreateBatch(decoderInputVariable.Shape, data, device);
+
+            var minibatch = new Dictionary<Variable, Value>
+            {
+                { decoderInputVariable, value},
+            };
+            return minibatch;
         }
     }
 }
