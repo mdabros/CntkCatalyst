bebdc04030ebe04ba3ee92b8381ffdfc9b82c063
mdabros <mdabros@users.noreply.github.com>
2019-07-25 23:05:45 +0200

    Add nuspec and update assemblyinfo

--- src/CntkCatalyst/Properties/AssemblyInfo.cs
+++ src/CntkCatalyst/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CntkCatalyst")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Deep learning API based on Microsoft CNTK.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mdabros")]
 [assembly: AssemblyProduct("CntkCatalyst")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Mads Dabros 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha.9")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha.10")]
